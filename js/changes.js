14e246e93bba4336ad0d869617335b6b8ca57e70
wadd0032 <deannawadden@rogers.com>
2012-02-10 14:52:45 -0500

    created vars, jquery for property changes and for hide/seek

--- js/changes.js
+++ js/changes.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+
+$(document).ready(function () {
+
+	var $circle = $('#circle');
+	var $properties = $('#properties');
+	var $colour = $('#colour');
+
+	$('form').on('submit', function (e) {
+		var colour = $colour.val();
+		e.preventDefault();
+
+		if (colour) {
+		$circle.css($properties.val(), colour);
+		}
+	});
+
+	$('#hide-show').on('click', function (e) {
+		$circle.toggle('slow');
+	});
+});
